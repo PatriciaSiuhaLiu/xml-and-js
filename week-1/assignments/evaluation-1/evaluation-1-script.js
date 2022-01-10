18c19634face949ad5895bced8d2deb3748a7e6f
Patricia Siuha Liu <liupatricia.18@gmail.com>
2022-01-10 13:43:06 -0500

    Evaluation-1

---
+++ week-1/assignments/evaluation-1/evaluation-1-script.js
@@ -0,0 +1,6 @@
+ function onclickhandler(){
+     const node = document.createElement("h1");
+     node.appendChild(document.createTextNode("Patricia Siuha Liu")); 
+     document.body.appendChild(node)
+   
+}
--- week-1/assignments/evaluation-1/script.js
+++
@@ -1,5 +0,0 @@
- function onclickhandler(){
-     const node = document.createElement("h1");
-     node.appendChild(document.createTextNode("Patricia")); 
-    document.append(node);
-}
